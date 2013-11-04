0089a9a6a90de6b5d617ddd35d16c984fbe0c13a
Francis Tseng <ftzeng@gmail.com>
2013-11-04 15:38:37 -0500

    initial commit

---
+++ m4a-renamer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Extracts m4a metadata and uses it to rename files.
+# Requires mutagen: pip install mutagen
+
+for file in *.m4a
+do
+    ALBUM="`mutagen-inspect "$file" | grep "alb=" | sed -e 's/.*=//g'`"
+    ARTIST="`mutagen-inspect "$file" | grep "ART=" | sed -e 's/.*=//g'`"
+    TITLE="`mutagen-inspect "$file" | grep "nam=" | sed -e 's/.*=//g'`"
+    mv $file "$ARTIST - $TITLE.m4a"
+done
